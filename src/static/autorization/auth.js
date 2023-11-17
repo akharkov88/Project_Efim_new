b42a0ed8b99c8fa18f80881382859308eebb76a0
akharkov88 <akharkov88@mail.ru>
2023-11-17 15:27:36 +0300

    0.0.4

--- src/static/autorization/auth.js
+++ src/static/autorization/auth.js
@@ -2,8 +2,21 @@ function autorization(){
 alert('123й4')
 }
 
-addEventListener("submit", (event) => {
+ addEventListener("submit", async(event) => {
 
     console.log('1')
     console.log(event)
+
+        event.preventDefault();
+
+    let response = await fetch('/article/formdata/post/user', {
+      method: 'POST',
+      body: new FormData(document.getElementById("formElem")
+)
+    });
+
+    let result = await response.json();
+
+    console.log(result.message);
+
 });
