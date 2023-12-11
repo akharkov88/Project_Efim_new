3aa8d8d9ca6ac17273138a5284cf85a063a818b5
akharkov88 <akharkov88@mail.ru>
2023-12-11 22:27:13 +0300

    11.12

--- tables_New_Create.py
+++ tables_New_Create.py
@@ -17,28 +17,16 @@ from sqlalchemy.sql import func
 
 Base = declarative_base()
 
-class TaskForm(Base):
-    __tablename__ = 'TechTaskForm'
 
-    id = Column(Integer, primary_key=True)
-    NameTechTask = Column(String, unique=True) # Наименование обекта
-    TechTaskClient = Column(String) # Наименование заказчика
-    TechTaskProject = Column(String) # Проект/чертеж
-    TechTaskPPR = Column(String) # Проект/чертеж
-    TechTaskOverhead = Column(String) # Накладные расходы
-    TechTaskDateKP = Column(String)#Column(Date) #  Срок подготовки КП
-    TechTaskDateEndWork = Column(String) #  Срок выполнение работ
-    TechTaskPrice = Column(String) #  Условия оплаты
-    TechTaskLeaderKP = Column(String) #  Отвественный
 # create_at = Column(DateTime(timezone=True), server_default=func.now())
 # update_at = Column(DateTime(timezone=True), onupdate=func.now())
-# class BaseFail(Base):
-#     __tablename__ = 'BaseFile_t'
-#     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-#     name = Column(String, nullable=False)
-#     create_at =  Column(DateTime(timezone=True), server_default=func.now())
-#     # create_at =  Mapped[create_at]
-#     # update_at =  Mapped[update_at]
+class BaseFail(Base):
+    __tablename__ = 'BaseFile_t'
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    create_at =  Column(DateTime(timezone=True), server_default=func.now())
+    # create_at =  Mapped[create_at]
+    # update_at =  Mapped[update_at]
 
 from  database import engine
 from  tables_New_Create import Base
