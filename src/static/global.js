606454eebb2077404f05e14fd198db38de078c38
akharkov88 <akharkov88@mail.ru>
2023-12-17 21:41:21 +0300

    17.12

--- src/static/global.js
+++ src/static/global.js
@@ -1,4 +1,5 @@
 async function check_user_func() {
+
     check_user = await fetch('/auth/user/', {
         headers: {
             Accept: 'application/json',
@@ -11,6 +12,56 @@ async function check_user_func() {
 }
 
 check_user_func()
+
+async function autroriz() {
+    // if (window.stop !== undefined) {
+    //     window.stop();
+    // } else if (document.execCommand !== undefined) { // для IE
+    //     document.execCommand("Stop", false);
+    // }
+
+    //works in all browsers but IE
+    // if ($.browser.msie) {
+    //     document.execCommand("Stop");
+    // }    ;
+
+        if (sessionStorage.getItem('stopload')) { // если в хранилище есть переменная 'stopload'
+        // sessionStorage.removeItem('stopload'); // удалить 'stopload' из хранилища
+    } else { // остановить загрузку HTML страницы
+        if (window.stop !== undefined) {
+            window.stop();
+        } else if (document.execCommand !== undefined) {
+            document.execCommand("Stop", false);
+        }
+    }
+    check_user = await fetch('/auth/user/', {
+        headers: {
+            Accept: 'application/json',
+            "Authorization": localStorage.getItem('Authorization')
+        },
+    });
+// debugger
+
+
+
+    console.log("check_user.status", check_user.status)
+    if (check_user.status != 200) {
+
+        document.location.href = '/auth/'
+    } else {
+        console.log("check_user.status", check_user.status)
+
+        if (sessionStorage.getItem('stopload')) { // если в хранилище есть переменная 'stopload'
+ sessionStorage.removeItem('stopload');
+        }else {
+                    sessionStorage.setItem('stopload', 'none'); // занести переменную в хранилище
+
+            location.reload();
+        }
+
+    }
+} // функция останавливает загрузку страницы
+
 
 function downloadFileFromURL(url, fileName) {
     const xhr = new XMLHttpRequest();
@@ -31,3 +82,17 @@ function downloadFileFromURL(url, fileName) {
 
     xhr.send();
 }
+
+
+document.getElementById('Settings Create_User').onclick = function (event) {
+    check_user = fetch('/auth/user/', {
+        headers: {
+            Accept: 'application/json',
+            "Authorization": localStorage.getItem('Authorization')
+        },
+    });
+    if (check_user.status != 200) {
+        document.location.href = '/auth/create'
+    }
+}
+
