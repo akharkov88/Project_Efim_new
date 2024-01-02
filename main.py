c755d61db95ceb184648cff35c453b23639f111a
akharkov88 <akharkov88@mail.ru>
2024-01-02 14:15:55 +0300

    02.01

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ from api import router
 from pathlib import Path
 from fastapi.staticfiles import StaticFiles
 import os
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 app.mount(
     "/static",
