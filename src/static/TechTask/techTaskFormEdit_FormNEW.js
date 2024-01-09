ab84cd1eaebf5c08c7047f302815593b0391ea40
akharkov88 <akharkov88@mail.ru>
2024-01-09 10:01:00 +0300

    09.01 10:00

--- src/static/TechTask/techTaskFormEdit_FormNEW.js
+++ src/static/TechTask/techTaskFormEdit_FormNEW.js
@@ -1,12 +1,17 @@
 $("#div1").delegate(":input", "input", function (e) {
     var options = $('datalist')[0].options;
-    if (options.length!=0 && options[options.length-1].value==$(this).val()){
+    if (options.length != 0 && options[options.length - 1].value == $(this).val()) {
+        if ($(this).val().indexOf("Добавить в список \"") != -1) {
+            set_setSuggest("type", $(this).val().replace("Добавить в список \"", "").slice(0, -1),"tetst_v")
+            $('#input_v').val($(this).val().replace("Добавить в список \"", "").slice(0, -1));
+        }
         return
     }
-    set_options_datalist("type", e.target.value, 5)
+    set_options_datalist("type", e.target.value,"tetst_v", 5)
 
 });
-function set_options_datalist(customer_id, search, count = 10) {
+
+function set_options_datalist(customer_id, search,id_datalist, count = 10) {
     $.ajax({
         method: 'POST',
         url: "/suggest/getSuggest",
@@ -18,26 +23,56 @@ function set_options_datalist(customer_id, search, count = 10) {
         data: `{"customer_id":"${customer_id}","search": "${search}","count":${count}}`,
         success: function (ingredients) {
             for (var i in ingredients) {
+
+                console.log("search",search)
+                console.log("ingredients[i].value_table",ingredients[i].value_table)
+
                 if (ingredients[i].value_table == search) {
+                console.log("return")
+
                     return
                 }
             }
-            $("#tetst_v").empty();
+                console.log("return2")
+
+            $(`#${id_datalist}`).empty();
             for (var i in ingredients) {
-                $(`<option id="${i}" />`).html(ingredients[i].value_table).appendTo("#tetst_v");
+                $(`<option id="${i}" />`).html(ingredients[i].value_table).appendTo(`#${id_datalist}`);
                 if (i == ingredients.length - 1) {
-                    $(`<option/>`).html(search).appendTo("#tetst_v");
+                    $(`<option/>`).html(`Добавить в список "${search}"`).appendTo(`#${id_datalist}`);
                 }
 
             }
         },
         error: function (error) {
-            $("#tetst_v").empty();
-            $(`<option/>`).html(search).appendTo("#tetst_v");
+            $(`#${id_datalist}`).empty();
+            $(`<option/>`).html(`Добавить в список "${search}"`).appendTo(`#${id_datalist}`);
 
         },
     });
 }
+
+function set_setSuggest(customer_id, add_value,id_datalist) {
+    $.ajax({
+        method: 'POST',
+        url: "/suggest/setSuggest",
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            "Authorization": localStorage.getItem('Authorization')
+        },
+        data: `{"customer_id":"${customer_id}","value_table": "${add_value}"}`,
+        success: function (ingredients) {
+            $(`#${id_datalist}`).empty();
+
+            set_options_datalist("type",add_value,5)
+
+        },
+        error: function (error) {
+        },
+    });
+}
+
 //
 // $("#input_v").delegate(":input", "input", function (event) {
 //     var _this = $(this);
