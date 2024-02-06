b73e264161b8f407dee361be0d92293f3b471724
Vladimir1360 <nvladv@mail.ru>
2024-02-06 14:36:09 +0300

    del total summ nds

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from fastapi.templating import Jinja2Templates
 from fastapi.exceptions import HTTPException
 
 
-import telebot
-from telebot import types
+# import telebot
+# from telebot import types
 import threading
 
 async def not_found_error(request: Request, exc: HTTPException):
@@ -68,64 +68,64 @@ app.mount(
 #
 #     return HTMLResponse(content=open("src/auth.html").read(), status_code=200)
 
-def telega():
-    bot = telebot.TeleBot('6699554023:AAFv2VuN2NcqydlFlkK5qCpLmCzLL3Euy_g')
-
-    # wikipedia.set_lang("ru")
-
-
-    alphabet = ' 1234567890-йцукенгшщзхъфывапролджэячсмитьбюёqwertyuiopasdfghjklzxcvbnm?%.,()!:;'
-
-
-    @bot.message_handler(commands=['start'])
-    def start_message(message):
-        print(message)
-        print(message)
-        bot.send_message(message.chat.id, "Здравствуйте, Сэр.")
-
-    question = ""
-
-    @bot.message_handler(content_types=['text'])
-    def get_text_messages(message):
-        command = message.text.lower()
-        if command == "не так":
-            bot.send_message(message.from_user.id, "а как?")
-            bot.register_next_step_handler(message, wrong)
-        else:
-            global question
-            question = command
-            # reply = "123"
-            # if reply == "вики ":
-            markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-            item1=types.KeyboardButton("Факт")
-            item2=types.KeyboardButton("Поговорка")
-            markup.add(item1)
-            markup.add(item2)
-            # bot.send_message(message.from_user.id, getwiki(command),  reply_markup=markup)
-            bot.send_message(message.from_user.id, "getwiki(command)",  reply_markup=markup)
-            # else:
-            #     bot.send_message(message.from_user.id, reply)
-
-
-    def wrong(message):
-        a = f"{question},{message.text.lower()} \n"
-        with open('dialogues.txt', "a", encoding='utf-8') as f:
-            f.write(a)
-        bot.send_message(message.from_user.id, "Готово")
-
-
-
-    bot.polling(none_stop=True)
-
-def telega_send_message(id,text):
-    bot = telebot.TeleBot('6699554023:AAFv2VuN2NcqydlFlkK5qCpLmCzLL3Euy_g')
-    bot.send_message(id, text)
+# def telega():
+#     bot = telebot.TeleBot('6699554023:AAFv2VuN2NcqydlFlkK5qCpLmCzLL3Euy_g')
+#
+#     # wikipedia.set_lang("ru")
+#
+#
+#     alphabet = ' 1234567890-йцукенгшщзхъфывапролджэячсмитьбюёqwertyuiopasdfghjklzxcvbnm?%.,()!:;'
+#
+#
+#     @bot.message_handler(commands=['start'])
+#     def start_message(message):
+#         print(message)
+#         print(message)
+#         bot.send_message(message.chat.id, "Здравствуйте, Сэр.")
+#
+#     question = ""
+#
+#     @bot.message_handler(content_types=['text'])
+#     def get_text_messages(message):
+#         command = message.text.lower()
+#         if command == "не так":
+#             bot.send_message(message.from_user.id, "а как?")
+#             bot.register_next_step_handler(message, wrong)
+#         else:
+#             global question
+#             question = command
+#             # reply = "123"
+#             # if reply == "вики ":
+#             markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+#             item1=types.KeyboardButton("Факт")
+#             item2=types.KeyboardButton("Поговорка")
+#             markup.add(item1)
+#             markup.add(item2)
+#             # bot.send_message(message.from_user.id, getwiki(command),  reply_markup=markup)
+#             bot.send_message(message.from_user.id, "getwiki(command)",  reply_markup=markup)
+#             # else:
+#             #     bot.send_message(message.from_user.id, reply)
+#
+#
+#     def wrong(message):
+#         a = f"{question},{message.text.lower()} \n"
+#         with open('dialogues.txt', "a", encoding='utf-8') as f:
+#             f.write(a)
+#         bot.send_message(message.from_user.id, "Готово")
+#
+#
+#
+#     bot.polling(none_stop=True)
+#
+# def telega_send_message(id,text):
+#     bot = telebot.TeleBot('6699554023:AAFv2VuN2NcqydlFlkK5qCpLmCzLL3Euy_g')
+#     bot.send_message(id, text)
 
 
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=telega, args=(), daemon=True)
-    t1.start()
+    # t1 = threading.Thread(target=telega, args=(), daemon=True)
+    # t1.start()
     # telega_send_message("622070505", "getwiki(command)")
     uvicorn.run(
         "main:app",
