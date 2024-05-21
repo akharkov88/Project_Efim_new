6e273037b31d6f9fdb569ad8ea45e7b1651ea022
akharkov88 <akharkov88@mail.ru>
2024-05-21 14:41:22 +0300

    departament

--- src/static/TechTask/techTask.js
+++ src/static/TechTask/techTask.js
@@ -6,7 +6,7 @@ checkbox_sketch_func = checkbox_sketch.onclick = function () {
         document.getElementById('div_checkbox_sketch').style.display = 'none'
     }
 }
-checkbox_sketch_func = checkbox_plan.onclick = function () {
+checkbox_plan_func = checkbox_plan.onclick = function () {
     console.log('press')
     if (checkbox_plan.checked) {
         document.getElementById('div_checkbox_plan').style.display = 'block'
